7556e508ceb97f883dd797b000d4336e6ca3bf7e
yuan15980721126 <457896654@qq.com>
2019-04-17 18:42:50 +0800

    删除 测试

--- index.php
+++ index.php
@@ -3,6 +3,4 @@
     define('UPLOAD_PATH','./Uploads/');
     define('VERSION','v2.2 Released');
     define('UPDATETIME','20121225');
-define('THINK_PATH','./Core/');
-require(THINK_PATH.'Core.php');
 ?>
